b4e7c5a18e7982ae05c375d2868093e28ce0c1bc
Moshe-Victor Solomon <moshevictor@yahoo.co.uk>
2025-02-25 15:49:27 +0200

    eslint fixes 2.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,4 @@
-import Link from "next/link";
 import RecentTransactions from "@/app/dashboard/transactions/recent-transactions";
-import {SearchParams} from "next/dist/server/request/search-params";
 import Cashflow from "@/app/dashboard/cashflow";
 
 export default async function DashboardPage({
@@ -10,7 +8,7 @@ export default async function DashboardPage({
 }) {
     const today = new Date();
     const searchParamsValues = await searchParams;
-    let cfYear = Number(searchParamsValues?.cfyear ?? today.getFullYear());
+    const cfYear = Number(searchParamsValues?.cfyear ?? today.getFullYear());
     return (
         <div className="max-w-screen-xl mx-auto py-5">
             <h1 className="text-4xl font-semibold pb-5">Dashboard</h1>
